f608d6b8a24c7a0d4d1bf5db3a01f9ee60d8cef6
Vladimir <info@migdex.ru>
2024-03-25 12:00:39 +0300

    added plus.py

--- kwuk.py
+++ kwuk.py
@@ -15,16 +15,16 @@ while bad_data == True:
     except:
         print('some error')
 
-D = b*b - (4*a*c)
+D = b * b - (4 * a * c)
 print(f'value = {D}')
 
 if D > 0:
     d = sqrt(D)
-    X1 = ((-b) + d)/(2*a)
-    X2 = ((-b) - d)/(2 * a)
+    X1 = ((-b) + d) / (2 * a)
+    X2 = ((-b) - d) / (2 * a)
     print(f'1 root = {X1}, 2 root = {X2}')
 elif D == 0:
-    X1 = (-b)/(2*a)
+    X1 = (-b) / (2 * a)
     print(f'1 root = {X1}')
 else:
     print('no roots')
---
+++ plus.py
@@ -0,0 +1,15 @@
+print('hello system')
+bad_data = True
+while bad_data == True:
+    try:
+        a = int(input('enter a: '))
+        b = int(input('enter b: '))
+        bad_data = False
+    except ValueError:
+        print('error')
+    except ZeroDivisionError:
+        print('zero error')
+    except:
+        print('some error')
+
+print(f'summ {a} and {b} = {a+b}')
