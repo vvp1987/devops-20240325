cabd169f4c4b1fd4af28a3f724e94f6ed30e18ce
Vladimir <info@migdex.ru>
2024-03-25 11:50:14 +0300

    import sqrt from math

--- kwuk.py
+++ kwuk.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 
 print("hello solving system")
 bad_data = True
@@ -19,7 +19,7 @@ D = b*b - (4*a*c)
 print(f'value = {D}')
 
 if D > 0:
-    d = math.sqrt(D)
+    d = sqrt(D)
     X1 = ((-b) + d)/(2*a)
     X2 = ((-b) - d)/(2 * a)
     print(f'1 root = {X1}, 2 root = {X2}')
